6d032281b84260e3eee5cf8c59b50937a99c8b71
Raphael Freitas <raphaeldefreitas2009@gmail.com>
2019-05-19 12:50:39 -0300

    Inicialização do Projeto

---
+++ PLAUTO-Board_Code.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		PLAUTO_Board_Code.ino
+ Created:	19/05/2019 12:48:49
+ Author:	rapha
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
